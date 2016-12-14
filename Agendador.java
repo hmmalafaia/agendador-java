fa14c4929e78d0617a8a6cb5500fd8501d6ea41a
hmmalafaia <hmmalafaia@gmail.com>
2016-12-13 22:26:32 -0200

    Refatoracao

--- Agendador.java
+++ Agendador.java
@@ -1,24 +1,19 @@
 import java.util.Calendar;
 public class Agendador{
   public boolean podeExecutar(int ano, int mes, int dia, int hora, int minuto, int segundo) throws InterruptedException{
-  Calendar dataAgendada = Calendar.getInstance();
-  dataAgendada.set(ano, mes-1, dia, hora, minuto, segundo);
-  System.out.println(dataAgendada.getTime());
-  boolean tentarNovamente = true;
-  while(tentarNovamente){
+    Calendar dataAgendada = Calendar.getInstance();
+    dataAgendada.set(ano, mes-1, dia, hora, minuto, segundo);
     Calendar dataAtual = Calendar.getInstance();
-      if(dataAtual.getTime().before(dataAgendada.getTime())){
+    if(dataAtual.getTime().before(dataAgendada.getTime())){
         System.out.println("Ainda nao chegou a hora");
         System.out.println("Agendado para: " + dataAgendada.getTime());
         System.out.println("Horario Atual: " + dataAtual.getTime());
-        System.out.println("Vou aguardar 60 segundos");
-        Thread.currentThread().sleep(60 * 1000);
-      } else {
+        return false;
+    } else {
         System.out.println("Chegou a hora");
-        tentarNovamente = false;
+        System.out.println("Agendado para: " + dataAgendada.getTime());
+        System.out.println("Horario Atual: " + dataAtual.getTime());
         return true;
-      }
-  }
-  return false;
+    }
   }
 }
--- Executa.java
+++ Executa.java
@@ -1,10 +1,10 @@
 public class Executa{
   public static void main(String[] args) throws InterruptedException{
     Agendador agendador = new Agendador();
-    if(agendador.podeExecutar(2016, 12, 12, 22, 55, 0)){
-      System.out.println("CHAMAR TAREFA XPTO");
-    } else {
-      System.out.println("Erro");
+                                 //Ano, Mes, Dia, Hora, Minuto, Segundo
+    while(!agendador.podeExecutar(2016, 12, 13, 22, 23, 0)){
+              Thread.currentThread().sleep(60 * 1000);
     }
+    System.out.println("TimesUP");
   }
 }
